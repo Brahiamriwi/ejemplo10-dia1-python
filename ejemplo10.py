35fd4a4dc460f2c29096702744621dfaf7c48f11
Brahiamriwi <brahiamruizalzate@gmail.com>
2025-04-21 21:57:52 -0500

    Add files via upload

---
+++ ejemplo10.py
@@ -0,0 +1,7 @@
+numero = int(input("Ingresa un numero inferior a 100: "))
+
+if numero<10 and numero >1:
+    print("Esta en el rango de 1 a 10")
+
+elif numero>10 and nuemro <100:
+    print("Esta por fuera del rango de 1 a 10") 
